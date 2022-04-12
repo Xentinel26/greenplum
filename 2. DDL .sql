39301e9ba91379b0304129a894a57c9a596fba2f
Julio César <jromero@cxc.com.mx>
2022-04-12 13:35:58 -0500

    Greenplum Reference guide and Table level Constraints

--- 2. DDL .sql
+++ 2. DDL .sql
@@ -47,3 +47,73 @@ where tablespace = 'fast_ssd_ts'
 drop table finance.tabla_ejemplo_ts;
 
 drop tablespace fast_ssd_ts;
+
+-- create table
+
+create table finance.empleado(
+    id bigserial ,
+    nombre varchar(60),
+    apellido_paterno varchar(60),
+    apellido_materno varchar(60),
+    fecha_nacimiento date,
+    pais    varchar(90),
+    -- Constraint Unique como un super conjunto de la llave de distribucion
+    unique (id, nombre, pais)
+) distributed by (id, nombre);
+
+
+drop table if exists finance.empleado;
+
+create table finance.empleado(
+    id bigserial ,
+    -- Unique Constraint coincide con la llave de distribución
+    rfc_homoclave varchar(60) unique,
+    nombre varchar(60),
+    apellido_paterno varchar(60),
+    apellido_materno varchar(60),
+    fecha_nacimiento date,
+    pais    varchar(90) check ( pais in ('MX', 'BOL') )
+) distributed by (rfc_homoclave);
+
+
+drop table if exists finance.empleado;
+
+create table finance.empleado(
+    id bigserial ,
+    nombre varchar(60) not null,
+    apellido_paterno varchar(60),
+    apellido_materno varchar(60),
+    fecha_nacimiento date,
+    pais    varchar(90),
+    -- Constraint Unique como un super conjunto de la llave de distribucion
+    unique (id, nombre, pais)
+) distributed by (id, nombre);
+
+drop table if exists finance.empleado;
+-- Foreign keys
+create table finance.empleado(
+    id bigserial unique ,
+    nombre varchar(60) not null,
+    apellido_paterno varchar(60),
+    apellido_materno varchar(60),
+    fecha_nacimiento date,
+    pais    varchar(90),
+    id_jefe bigint references finance.empleado(id)
+) distributed by (id);
+
+select *
+from finance.empleado
+;
+ -- Not enforcing foreign key
+INSERT INTO finance.empleado (id, nombre, apellido_paterno, apellido_materno, fecha_nacimiento, pais, id_jefe)
+VALUES (1, 'Julio', 'Romero', 'Perez', '1992-04-02', 'México', 1);
+INSERT INTO finance.empleado (id, nombre, apellido_paterno, apellido_materno, fecha_nacimiento, pais, id_jefe)
+VALUES (2, 'Alejandra', 'González', 'Hernández', '1992-05-14', 'México', 3);
+
+
+
+
+
+
+show gp_create_table_random_default_distribution;
+
